e7ddf8a6585268fb10a53a3f71266feadf2705cd
TheNoim <nilsbergmann@noim.io>
2016-10-25 21:14:17 +0200

    auto-install

---
+++ dev-auto-install.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo Start auto-install...
+call "node_modules/.bin/auto-install.cmd"
--- dev-auto-install.sh
+++ dev-auto-install.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+echo "Start Auto auto-install..."
+node_modules/.bin/auto-install
