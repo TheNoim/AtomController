b5974e03e00acfe5a918e7464b2d4e11dfb55492
TheNoim <nilsbergmann@noim.io>
2016-10-25 20:01:07 +0200

    Install output
    Added installation output via command prompt

--- install-all.cmd
+++ install-all.cmd
@@ -1,4 +1,8 @@
+@echo off
+echo Install Node dependencies via npm
 call npm install
+echo Run electron rebuild
 call electron-rebuild.cmd
+echo Install Bower dependencies
 call install-bower.cmd
 PAUSE
--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+function pause(){
+   read -p "$*"
+}
+echo "Install Node dependencies via npm."
 npm install
+echo "Run electron rebuild."
 ./electron-rebuild.sh
+echo "Install Bower dependencies."
 ./install-bower.sh
+pause 'Press [Enter] key to close...'
