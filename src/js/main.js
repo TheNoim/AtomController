6aee520d31083844fef758ea3977e6e5f3e6a456
TheNoim <nilsbergmann@noim.io>
2016-10-28 13:32:12 +0200

    Ng-FX

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 /**
  * Created by nilsbergmann on 25/10/2016.
  */
-const ang = angular.module('AtomApp', ['ngMaterial']);
+const ang = angular.module('AtomApp', ['ngMaterial', 'ngAnimate', 'ngFx', 'ui.router']);
 ang.controller('AtomApp', ($scope) => {
 
+});
+ang.config(($stateProvider,$urlRouterProvider) => {
+
+    $urlRouterProvider.otherwise('/loading');
+
+    const loading = {
+        url: '/loading'
+    };
+
+
+    $stateProvider.state('loading', loading);
 });
